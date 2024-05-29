23cb4a20249f27bad39d2b8518ca64417e0713d1
Claas <claas@claas.dev>
2024-05-29 21:42:39 +0200

    Delete all the jazz

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  trailingComma: "all",
-  tabWidth: 2,
-  singleQuote: false,
-  plugins: ["prettier-plugin-tailwindcss"],
-};
--- src/components/test.tsx
+++
@@ -1,9 +0,0 @@
-import * as wasm from "../../rust/pkg/meal";
-import { createResource } from "solid-js";
-import { clientOnly } from "@solidjs/start";
-
-export default function App() {
-  // const initialization = createResource(init);
-  const result = wasm.add(60, 9);
-  return <>{result}</>;
-}
