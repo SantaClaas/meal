8c3b7d2e8e212ea827940aa9803be3339d50eabb
Claas <claas@claas.dev>
2024-09-29 22:46:46 +0200

    Create tool to pre compress static files

---
+++ pumpe/README.md
@@ -0,0 +1,3 @@
+# An air compressor is just a pump
+
+A tool to precompress files to reduce download sizes. It uses the [tower-http compression module](https://docs.rs/tower-http/latest/tower_http/compression/index.html) to achieve the same compression results as if it would be compressed on the fly.
---
+++ pumpe/src/main.rs
@@ -0,0 +1,155 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    env,
+    fmt::format,
+    fs,
+    path::{Path, PathBuf},
+    rc::Rc,
+    sync::Arc,
+};
+
+use axum::{
+    body::{Body, Bytes},
+    extract::{self, path, State},
+    http::{self, header, request},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use camino::Utf8PathBuf;
+use clap::{error::ErrorKind, CommandFactory, Parser};
+use http_body_util::BodyExt;
+use thiserror::Error;
+use tower::{ServiceBuilder, ServiceExt};
+use tower_http::{
+    compression::{Compression, CompressionLayer},
+    services::ServeDir,
+};
+
+#[derive(Parser, Debug)]
+#[command()]
+struct Arguments {
+    #[arg(num_args = 1)]
+    directory: Utf8PathBuf,
+}
+
+#[derive(Error, Debug)]
+enum PumpeError {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+}
+
+fn visit_directory(path: Utf8PathBuf) -> Result<Rc<[Utf8PathBuf]>, PumpeError> {
+    let mut files = Vec::new();
+    let mut directory_queue = VecDeque::new();
+    directory_queue.push_back(path.clone());
+
+    // Breadth first traversal
+    while let Some(path) = directory_queue.pop_front() {
+        for entry in path.read_dir_utf8()? {
+            let entry = entry?;
+            let path = entry.path().to_path_buf();
+            if path.is_dir() {
+                directory_queue.push_back(path);
+
+                continue;
+            }
+
+            files.push(path);
+        }
+    }
+
+    Ok(files.into())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), PumpeError> {
+    // Helpful: https://www.rustadventure.dev/introducing-clap/clap-v4/accepting-file-paths-as-arguments-in-clap
+    let directory = Arguments::parse().directory;
+
+    if !directory.exists() {
+        let mut command = Arguments::command();
+        command.error(
+            ErrorKind::ValueValidation,
+            format!("Path does not exist: {}", directory),
+        );
+    }
+
+    if !directory.is_dir() {
+        let mut command = Arguments::command();
+        command.error(
+            ErrorKind::ValueValidation,
+            format!("Path is not a directory: {}", directory),
+        );
+    }
+
+    // List all files in directory
+    let files = visit_directory(directory.clone())?;
+
+    //TODO use service builder
+    let app: Router = Router::new()
+        // .fallback_service(ServeDir::new(arguments.directory.as_path()))
+        .fallback_service(ServeDir::new(directory.clone()))
+        .layer(CompressionLayer::new());
+
+    // app.map_request()
+    // let request = http::Request::get("/app/dist/index.html")
+    //     .header(header::CONTENT_ENCODING, "gzip")
+    //     .body(Body::empty())
+    //     .unwrap();
+    // let result = app.clone().oneshot(request).await;
+    // println!("Result: {:?}", result);
+
+    // Strip source path as that is used as the root to serve files from
+    // for file in files.iter_mut() {
+    //     *file = file.strip_prefix(&path).unwrap().to_path_buf();
+    // }
+
+    fn request(path: &str, encoding: &str) -> http::Request<Body> {
+        http::Request::get(path)
+            .header(header::ACCEPT_ENCODING, encoding)
+            .body(Body::empty())
+            .unwrap()
+    }
+
+    async fn write_response(response: http::Response<Body>, target: &Utf8PathBuf) {
+        let data = response.into_body().collect().await.unwrap().to_bytes();
+        fs::write(target, data).unwrap();
+    }
+
+    async fn compress(
+        app: Router,
+        file: &Utf8PathBuf,
+        target_directory: impl AsRef<Path>,
+        (encoding_extension, encoding): (&str, &str),
+    ) {
+        // Request
+        let path = file.strip_prefix(target_directory).unwrap();
+        let path = Utf8PathBuf::from("/").join(path);
+        let request = request(path.as_str(), encoding);
+        let response = app.clone().oneshot(request).await.unwrap();
+
+        // Write
+        let mut target = file.clone();
+        let extension = target.extension().unwrap_or_default();
+        target.set_extension(format!("{extension}.{encoding_extension}",));
+        write_response(response, &target).await;
+    }
+
+    for file in files.iter() {
+        // Detect if file is already compressed. This is not precise as it does not check the content but it is enough for our usecase
+        if file
+            .extension()
+            .is_some_and(|extension| matches!(extension, "br" | "gz" | "zst" | "zz"))
+        {
+            continue;
+        }
+
+        compress(app.clone(), file, directory.as_path(), ("gz", "gzip")).await;
+        compress(app.clone(), file, directory.as_path(), ("br", "br")).await;
+        compress(app.clone(), file, directory.as_path(), ("zst", "zstd")).await;
+        compress(app.clone(), file, directory.as_path(), ("zz", "deflate")).await;
+    }
+
+    Ok(())
+}
