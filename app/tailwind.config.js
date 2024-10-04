194a39ad893c34f6de954bb64066dcf6a7337b2f
Claas <claas@claas.dev>
2024-10-05 00:30:33 +0200

    Switch to plugin for more flexibility

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,6 @@
 import materialTailwind from "@claas.dev/material-tailwind";
 import formsPlugin from "@tailwindcss/forms";
+import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,5 +13,38 @@ export default {
     materialTailwind({
       source: "#0f172a",
     }),
+    plugin(
+      ({ addVariant, addUtilities }) => {
+        // The variant makes this usable with all Tailwind CSS classes that you might want to trigger when the parent
+        // container is scrolled
+        // "true" is not a keyword in CSS so this is just checking for the value any other value than "true" will not trigger this
+        addVariant("is-scrolled", "@container style(--is-scrolled: true)");
+        // Have to use "animate-..." or it won't include the keyframes
+        addUtilities({
+          ".animate-scroll-trigger": {
+            // animation: "scroll-trigger linear both",
+            animationRange: "0 1px",
+            animationTimeline: "scroll()",
+          },
+        });
+      },
+      {
+        theme: {
+          extend: {
+            // Tailwind CSS seems to not include the keyframes if they are not included in an animation
+            animation: {
+              "scroll-trigger": "scroll-trigger linear both",
+            },
+            keyframes: {
+              "scroll-trigger": {
+                to: {
+                  "--is-scrolled": "true",
+                },
+              },
+            },
+          },
+        },
+      }
+    ),
   ],
 };
