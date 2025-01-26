52074334534c6b2bf583474e2806bd4b800623ed
Claas <claas@claas.dev>
2025-01-26 19:42:42 +0100

    Limit rate limiting to machine endpoint
    as the traefik configured rate limit will be removed

--- tugboat/src/route/mod.rs
+++ tugboat/src/route/mod.rs
@@ -3,16 +3,19 @@ mod middleware;
 pub(crate) mod project;
 mod token;
 
-use askama_axum::IntoResponse;
+use std::time::Duration;
+
 pub(crate) use container::collect_garbage;
 
 use axum::{
-    debug_handler,
+    error_handling::HandleErrorLayer,
+    http::StatusCode,
     response::Redirect,
     routing::{get, post},
-    Router,
+    BoxError, Router,
 };
 use libsql::Connection;
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 
 use crate::TugState;
 
@@ -24,10 +27,23 @@ pub(super) struct Routes {
 pub(super) fn get_for_machines(connection: Connection) -> Router<TugState> {
     Router::new().route(
         "/containers/:container_id/update",
-        post(container::update).route_layer(axum::middleware::from_fn_with_state(
-            connection,
-            middleware::require_container_token,
-        )),
+        post(container::update)
+            .route_layer(axum::middleware::from_fn_with_state(
+                connection,
+                middleware::require_container_token,
+            ))
+            // Rate limiting based on https://github.com/tokio-rs/axum/discussions/987#discussioncomment-2678595
+            .route_layer(
+                ServiceBuilder::new()
+                    .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                        (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled error: {}", err),
+                        )
+                    }))
+                    .layer(BufferLayer::new(1024))
+                    .layer(RateLimitLayer::new(10, Duration::from_secs(60))),
+            ),
     )
 }
 pub(super) fn get_for_humans() -> Router<TugState> {
