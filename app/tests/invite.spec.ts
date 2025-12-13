6d8757a54c7b9b01944fea56a03089db5adf264f
Claas <claas@claas.dev>
2025-12-13 23:52:52 +0100

    Get basic copy test to work
    after this taking for ever to get working

---
+++ app/tests/invite.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect, Page } from "@playwright/test";
+import { GenericContainer } from "testcontainers";
+
+// test.beforeEach(async () => {
+//   const container = await GenericContainer.fromDockerfile(
+//     "..",
+//     "Dockerfile"
+//   ).build();
+//   const instance = await container.start();
+// });
+
+async function tabForward(page: Page, times: number) {
+  for (let i = 0; i < times; i++) {
+    await page.keyboard.press("Tab");
+  }
+}
+
+async function readClipboard(page: Page) {
+  return await page.evaluate(async () => {
+    const clipboard = await navigator.clipboard.readText();
+    console.log("CLIPBOARD", clipboard);
+    return clipboard;
+  });
+}
+
+test("A user can start a chat with another user", async ({ page, context }) => {
+  await page.goto("http://localhost:5173/invite");
+
+  // Page ready
+
+  const copyButton = page.getByText("Copy");
+  // await page.locator("button:has-text('Copy')").waitFor({ state: "attached" });
+  await copyButton.waitFor({ state: "visible" });
+
+  // Go to copy button
+  // await tabForward(page, 3);
+  // const copyButton = page.locator("*:focus");
+  // const copyButton = page.getByText("Copy");
+  // expect(copyButton).toHaveText("Copy");
+  // await page.keyboard.press("Enter");
+  await copyButton.click();
+
+  // await context.grantPermissions(["clipboard-read"]);
+  const invite = await readClipboard(page);
+  // Starts with "http://localhost:5173/join/"
+  expect(invite).toMatch(/^http:\/\/localhost:5173\/join\//);
+});
